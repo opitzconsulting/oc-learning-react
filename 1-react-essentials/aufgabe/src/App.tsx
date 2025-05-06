70d47790e0a8a9b9dbecdad1affc0fb29ecb6feb
Waldemar Lehner <waldemar.lehner@opitz-consulting.com>
2025-05-06 22:26:20 +0200

    misc: lösung

--- 1-react-essentials/aufgabe/src/App.tsx
+++ 1-react-essentials/aufgabe/src/App.tsx
@@ -1,19 +1,61 @@
-import './App.scss'
-import {loadBooks} from "./dummy-data/books.ts";
+import { useState } from 'react';
+import './App.scss';
+import { loadBooks } from './dummy-data/books.ts';
 
 function App() {
-    const allAvailableBooks = loadBooks();
+  const [selectedBookIds, setSelectedBookIds] = useState<
+    Record<string, boolean>
+  >({});
+  const allAvailableBooks = loadBooks();
 
-    return (
-        <>
-            <h2>Books-To-Read</h2>
-            {/* TODO: Display a box with all selected books here */}
+  const totalSelected = Object.values(selectedBookIds).filter(Boolean).length;
 
-            <div className="books-list">
-                {/* TODO: Display a list of all available books here */}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h2>Books-To-Read</h2>
+      <div className="card">
+        {totalSelected === 0 ? (
+          <p>Du hast noch keine Pläne...</p>
+        ) : (
+          <div>
+            Du willst die folgenden Bücher lesen:
+            <ul>
+              {allAvailableBooks
+                .filter((e) => selectedBookIds[e.id])
+                .map((e) => (
+                  <ul key={e.id}>{e.title}</ul>
+                ))}
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className="books-list">
+        {allAvailableBooks.map((e) => (
+          <div className="book card">
+            <span key={e.id} className="book-title">
+              {e.title} -
+            </span>
+            {e.author} -<span className="book-genre">{e.genre}</span>
+            <button
+              onClick={() => {
+                const newState = {
+                  ...selectedBookIds,
+                };
+                newState[e.id] = !newState[e.id];
+
+                setSelectedBookIds(newState);
+              }}
+              className="book-button"
+              disabled={!selectedBookIds[e.id] && totalSelected >= 3}
+            >
+              {selectedBookIds[e.id] ? 'Entfernen' : 'Lesen!'}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
