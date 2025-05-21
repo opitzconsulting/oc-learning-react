60b6ad36c470ff3f1ba99f810b81fa3a5dd9ba6b
Waldemar Lehner <waldemar.lehner@opitz-consulting.com>
2025-05-21 12:58:33 +0200

    lösung: bonusaufgabe

--- 3-routing/spa-routing/src/main.tsx
+++ 3-routing/spa-routing/src/main.tsx
@@ -4,15 +4,54 @@ import './index.css';
 import Home from './routes/Home.tsx';
 import Detail from './routes/Detail.tsx';
 import { NotFound } from './routes/NotFound.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter, Outlet, Route, Routes, useParams } from 'react-router';
+
+export function RootLayout() {
+  return (
+    <div
+      style={{
+        padding: '20px',
+        backgroundColor: '#111',
+        borderColor: 'red',
+        borderRadius: '10px',
+      }}
+    >
+      <Outlet />
+    </div>
+  );
+}
+
+export function UsersLayout() {
+  return (
+    <div>
+      <h1>Users Data</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function UsersNotFound() {
+  const params = useParams();
+  return (
+    <div>
+      <h1>User Not Found</h1>
+      <p>{params['*']!}</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/users/:id" element={<Detail />} />
-        <Route path="/*" element={<NotFound />} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route element={<UsersLayout />}>
+            <Route path="/users/:id" element={<Detail />} />
+            <Route path="/users/*" element={<UsersNotFound />} />
+          </Route>
+          <Route path="/*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
